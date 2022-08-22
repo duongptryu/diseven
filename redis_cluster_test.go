f6882fd7e8a5d8748dd16b8db76ad40693f2bda2
duong.phan <duong.phan@vieon.vn>
2022-08-22 23:02:14 +0700

    Add testing with redis cluster

---
+++ redis_cluster_test.go
@@ -0,0 +1,92 @@
+package diseven
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"testing"
+	"time"
+)
+
+var (
+	redisDbKVRead  *redis.ClusterClient
+	redisDbKVWrite *redis.ClusterClient
+)
+
+var addrHostsRead = []string{":7000", ":7001", ":7002", ":7003", ":7004", ":7005"}
+var addrHostsWrite = []string{":7000", ":7001", ":7002"}
+var ctx = context.Background()
+
+func init() {
+	redisDbKVRead = redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs:         addrHostsRead,
+		DialTimeout:   10 * time.Second,
+		ReadTimeout:   30 * time.Second,
+		WriteTimeout:  30 * time.Second,
+		RouteRandomly: true,
+	})
+
+	redisDbKVWrite = redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs: addrHostsWrite,
+		DialTimeout:  10 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		PoolSize:     10,
+		PoolTimeout:  30 * time.Second,
+	})
+
+	// check Redis
+	//if redisDbKVRead.Incr(context.Background(), "REDIS_PING").Val() <= 0 {
+	//	log.Fatalln("redisDbKVRead not valid")
+	//}
+	//
+	//if redisDbKVWrite.Incr(context.Background(), "REDIS_PING").Val() <= 0 {
+	//	log.Fatalln("redisDbKVWrite not valid")
+	//}
+
+	log.Println("Connected to redis cluster")
+}
+
+func Test(t *testing.T) {
+	if err := redisDbKVWrite.Set(ctx, "key", "value", 0).Err(); err != nil {
+		t.Error(err)
+		return
+	}
+
+	assert.Equal(t, "value", redisDbKVRead.Get(ctx, "key").Val())
+	log.Println("Set Success")
+}
+
+func BenchmarkPutKey(b *testing.B) {
+	d := mustNewDisEvent(3)
+	var num = 0
+	for num < 10000 {
+		keyHashTag := d.GenKeyWithHashTag(fmt.Sprintf("key%d", num))
+		if err := redisDbKVWrite.MSet(ctx, keyHashTag, fmt.Sprintf("key%d", num), 0).Err(); err != nil {
+			b.Error(err)
+			return
+		}
+		num++
+	}
+}
+
+func BenchmarkGetKey(b *testing.B) {
+	d := mustNewDisEvent(3)
+	var num = 0
+	var keys []string
+	for num < 10000 {
+		keys = append(keys, fmt.Sprintf("key%d", num))
+		num++
+	}
+	objectKeyHash := d.GetMultiKeyQuery(keys)
+	for _, v := range objectKeyHash {
+		resp := redisDbKVRead.MGet(ctx, v...)
+		if err := resp.Err(); err != nil {
+			b.Error(err)
+			continue
+		}
+		log.Println(resp.Val())
+	}
+}
